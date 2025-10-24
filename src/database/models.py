2991a5a4bcabb204c71f312734e9c04fa234aace
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-24 21:46:14 +0300

    Created User model

---
+++ src/database/models.py
@@ -0,0 +1,11 @@
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    __tablename__ = "users"
+    pass
