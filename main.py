96dd485a0d2f7c2765855dfb72318af492397d9d
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-26 00:11:48 +0300

    Restructured repo

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
+from src.api.health.routes import health_router
 from src.auth.routes import auth_router
-from src.database.connection import create_db, create_tables
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db()
-    await create_tables()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(auth_router)
+app.include_router(health_router)
