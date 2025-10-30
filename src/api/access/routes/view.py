d3f167b68190f253e578a40c161a11b5415b4d8b
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-30 15:12:42 +0300

    Removed auth dependancy

--- src/api/access/routes/view.py
+++ src/api/access/routes/view.py
@@ -6,9 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.access.schemas.view import ViewPermissionsResponse, ViewResourcesResponse, ViewUserResponse
 from src.api.access.services.view import AccessViewService
-from src.auth.manager import current_active_user
 from src.database.connection import get_async_session
-from src.database.models import User
 
 router = APIRouter(prefix="/view")
 
@@ -17,7 +15,6 @@ router = APIRouter(prefix="/view")
 async def view_user_groups(
     user_id: UUID,
     session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
 ):
     service = AccessViewService()
     return await service.view_user_groups(session, user_id)
@@ -27,48 +24,31 @@ async def view_user_groups(
 async def view_user_permissions(
     user_id: UUID,
     session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
 ):
     service = AccessViewService()
     return await service.view_user_permissions(session, user_id)
 
 
 @router.get("/user-resources", response_model=list[ViewResourcesResponse])
-async def view_user_resources(
-    user_id: UUID,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_user_resources(user_id: UUID, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_user_resources(session, user_id)
 
 
 @router.get("/group-users", response_model=list[ViewUserResponse])
-async def view_group_users(
-    group_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_group_users(group_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_group_users(session, group_id)
 
 
 @router.get("/group-permissions", response_model=list[ViewPermissionsResponse])
-async def view_group_permissions(
-    group_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_group_permissions(group_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_group_permissions(session, group_id)
 
 
 @router.get("/group-resources", response_model=list[ViewResourcesResponse])
-async def view_group_resources(
-    group_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_group_resources(group_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_group_resources(session, group_id)
 
@@ -77,57 +57,36 @@ async def view_group_resources(
 async def view_permission_groups(
     permission_id: PositiveInt,
     session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
 ):
     service = AccessViewService()
     return await service.view_permission_groups(session, permission_id)
 
 
 @router.get("/permission-users", response_model=list[ViewUserResponse])
-async def view_permission_users(
-    permission_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_permission_users(permission_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_permission_users(session, permission_id)
 
 
 @router.get("/permission-resources", response_model=list[ViewResourcesResponse])
-async def view_permission_resources(
-    permission_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_permission_resources(permission_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_permission_resources(session, permission_id)
 
 
 @router.get("/resource-permissions", response_model=list[ViewPermissionsResponse])
-async def view_resource_permissions(
-    resource_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_resource_permissions(resource_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_resource_permissions(session, resource_id)
 
 
 @router.get("/resource-groups", response_model=list[ViewPermissionsResponse])
-async def view_resource_groups(
-    resource_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_resource_groups(resource_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_resource_groups(session, resource_id)
 
 
 @router.get("/resource-users", response_model=list[ViewUserResponse])
-async def view_resource_users(
-    resource_id: PositiveInt,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_user),
-):
+async def view_resource_users(resource_id: PositiveInt, session: AsyncSession = Depends(get_async_session)):
     service = AccessViewService()
     return await service.view_resource_users(session, resource_id)
